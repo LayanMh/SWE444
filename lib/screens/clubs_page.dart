5970cf7abfc6c9c92fc6a4f3aea12daaae0d3802
JoudyMazen <162804131+JoudyMazen@users.noreply.github.com>
2025-11-07 19:40:38 +0300

    Update clubs_page.dart

--- lib/screens/clubs_page.dart
+++ lib/screens/clubs_page.dart
@@ -66,6 +66,11 @@ class _ClubFormPageState extends State<ClubFormPage> {
     _hoursController = TextEditingController(text: widget.existingItem?['hours']?.toString() ?? '');
     _descriptionController = TextEditingController(text: widget.existingItem?['description'] ?? '');
     _existingCertificateUrl = widget.existingItem?['certificateUrl'];
+
+    _titleController.addListener(() => setState(() {}));
+    _organizationController.addListener(() => setState(() {}));
+    _roleController.addListener(() => setState(() {}));
+    _descriptionController.addListener(() => setState(() {}));
   }
 
   @override
